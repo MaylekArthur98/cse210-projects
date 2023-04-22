2554f9f13c8a906a7a6c6d9fc21998167edcaf16
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-04-22 19:30:17 -0400

    prep 2 core req 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percent? ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (grade >=80 && grade < 90)
+        {
+             Console.WriteLine("You got an B!");
+        }
+        else if (grade >=70 && grade < 80)
+        {
+             Console.WriteLine("You got an C!");
+        }
+        else if (grade >=60 && grade < 70)
+        {
+             Console.WriteLine("You got an D!");
+        }
+        else 
+        {
+             Console.WriteLine("You got an F.");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("You've passed Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You've failed, try again.");
+        }
     }
 }

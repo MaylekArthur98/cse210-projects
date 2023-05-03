91cae920835e92bb116953e8566f4126b1515293
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-05-03 14:56:20 -0400

    Finished

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,11 +1,10 @@
-using System;
 public class Job
 {
     public string _jobTitle;
     public string _company;
     public int _startYear;
     public int _endYear;
-    public void _display()
+    public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}.");
     }

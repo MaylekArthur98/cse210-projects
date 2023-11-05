8ac28ed1456b6a2bcae5d93d3946e804b27966c0
MaylekArthur98 <Maylek.arthur@gmail.com>
2023-11-04 20:14:09 -0400

    slight changes

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -9,6 +9,8 @@ class Program
 // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
 // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
 // DisplayResult - Accepts the user's name and the squared number and displays them.
+        
+        
         DisplayWelcome();
         string name = PromptUserName();
         int number = PromptUserNumber();
